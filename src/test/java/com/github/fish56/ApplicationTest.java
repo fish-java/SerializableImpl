bb7d0778bb3aad64aabefa6b4b925b1470543ea1
bitfishxyz <bitfishxyz@gmail.com>
2019-05-21 20:11:01 +0800

    添加反序列化测试

--- src/test/java/com/github/fish56/ApplicationTest.java
+++ src/test/java/com/github/fish56/ApplicationTest.java
@@ -1,10 +1,13 @@
 package com.github.fish56;
 
+import com.alibaba.fastjson.JSONObject;
 import com.github.fish56.entity.Fish;
 import com.github.fish56.entity.Sheep;
 import org.junit.Test;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class ApplicationTest {
@@ -14,7 +17,7 @@ public class ApplicationTest {
      * @throws Exception
      */
     @Test
-    public void serial() throws Exception{
+    public void serialFish() throws Exception{
         Fish fish = new Fish(23, "Jon");
 
         //输出流保存的文件名为 my.out ；ObjectOutputStream能把Object输出成Byte流
@@ -37,5 +40,14 @@ public class ApplicationTest {
         oos.writeObject(sheep);
         oos.flush();
         oos.close();
+    }
+
+    @Test
+    public void parseFish() throws Exception{
+        ObjectInputStream ois = new ObjectInputStream(
+                new FileInputStream("sheep.out"));
+        Sheep sheep = (Sheep) ois.readObject();
+        System.out.println(JSONObject.toJSONString(sheep));
+        // {"id":23,"name":"Jon"}
     }
 }
